1e74854516d9f8c1e165a4d5ca7442e8e9c14dee
Douglas C Graham <85527825+DouglasGraham-SS@users.noreply.github.com>
2021-09-19 23:51:35 -0500

    Update roulette_dealer_finder_by_time_and_game.sh

--- Bash Scripts/Player_Dealer_Correlation/roulette_dealer_finder_by_time_and_game.sh
+++ Bash Scripts/Player_Dealer_Correlation/roulette_dealer_finder_by_time_and_game.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Requires 0310_Dealer_schedule. Arguments: 0310, 2 (single digit time), AM (AM or PM), 1 (1,2, or 3 depending on the game)
-## Example: "bash roulette_dealer_finder_by_time_and_game.sh 0310 2 AM 1
+## Example: "bash roulette_dealer_finder_by_time_and_game.sh 0310 2 AM 1"
 
 if [ $4 = '1' ]; then 
 	awk -F ' ' '{print $1, $2, $3, $4}' $1_Dealer_schedule | grep -i $2':00:00 '$3 && echo "Time and Blackjack dealer name."
