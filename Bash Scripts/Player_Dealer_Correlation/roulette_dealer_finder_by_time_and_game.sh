3079352be7d4851ec1b9fc05b4089186bd96317d
Douglas C Graham <85527825+DouglasGraham-SS@users.noreply.github.com>
2021-09-19 23:51:19 -0500

    Update roulette_dealer_finder_by_time_and_game.sh

--- Bash Scripts/Player_Dealer_Correlation/roulette_dealer_finder_by_time_and_game.sh
+++ Bash Scripts/Player_Dealer_Correlation/roulette_dealer_finder_by_time_and_game.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## Need 0310_Dealer_schedule. Arguments: 0310, 2 (single digit time), AM (AM or PM), 1 (1,2, or 3 depending on the game)
+## Requires 0310_Dealer_schedule. Arguments: 0310, 2 (single digit time), AM (AM or PM), 1 (1,2, or 3 depending on the game)
 ## Example: "bash roulette_dealer_finder_by_time_and_game.sh 0310 2 AM 1
 
 if [ $4 = '1' ]; then 
